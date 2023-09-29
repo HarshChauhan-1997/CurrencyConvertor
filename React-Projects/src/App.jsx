6f699fb260ed1dfcf1078581a0098ad44b3dc75a
HarshChauhan-1997 <143804989+HarshChauhan-1997@users.noreply.github.com>
2023-09-29 15:13:29 +0530

    CurrencyConvertor

---
+++ React-Projects/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CurrencyConverter } from './Componant/CurrencyConverterNew/CurrencyConverter'
+
+
+
+function App() {
+
+  return (
+    <>
+    <CurrencyConverter/>
+    </>
+  )
+}
+
+export default App
---
+++ React-Projects/src/Componant/CurrencyConverterNew/Hook.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export const Hook = (currency) => {
+    const [data, setdata] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setdata(res[currency]))
+        console.log(currency);
+        console.log(data);
+    }, [currency])
+    return data;
+}
